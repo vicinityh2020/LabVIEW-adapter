68656f41582763166f41cfa49d3650a8dd577d8c
YajuanGuan <37073115+YajuanGuan@users.noreply.github.com>
2018-10-23 17:03:19 +0200

    Update aau_adapter_1.0.0.py

--- aau_adapter_1.0.0.py
+++ aau_adapter_1.0.0.py
@@ -7,12 +7,14 @@ import json
 import time
 import threading
 
+#define global vars
 senddata1 = b'F'
 senddata2 = b'F'
 senddata3 = b'F'
 stopflag = 0
 
-
+#Enquire Parking price and free number from EMS
+#Publish events to subscribers through VICINITY agnet
 def Timerfun():
    global senddata1
    global senddata2
@@ -21,7 +23,7 @@ def Timerfun():
    #Calculate number of free parking slot 
    FreeSlot= senddata1 + senddata2 + senddata3 
    FreeSlot_string = str(FreeSlot, encoding = "utf-8")            
-   FreeSlotNum = FreeSlot_string.count('F')     
+   FreeSlotNum = FreeSlot_string.count('O')     
    FreeSlotNum_byte = bytes(str(FreeSlotNum), encoding = "utf8")
     
    #Inquire from Labview for Charging price
@@ -40,7 +42,6 @@ def Timerfun():
 
    payload = b'{' + b'"value":"' + Price + b'","free":"' +  FreeSlotNum_byte + b'","time":"'+ systemtime +b'"}'
    r=requests.request('PUT',url,headers=hd,data = payload)
-   print(r.text)
    
    t=threading.Timer(5,Timerfun,())   
    
@@ -50,7 +51,7 @@ def Timerfun():
         t.start()
 
 
-
+#Handle the http requests from VICINITY agent 
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
  
     def do_GET(self):
@@ -145,9 +146,7 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
            self.wfile.write(b'}')     
             
         else:
-            self.wfile.write(b'HTTP/1.1 406 Failed')   
-           
- 
+            self.wfile.write(b'HTTP/1.1 406 Failed')             
  
     def do_POST(self):
         
@@ -155,21 +154,17 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         
         content_length = int(self.headers['Content-Length'])
         body = self.rfile.read(content_length)
+        
         self.send_response(200)
         self.end_headers()
-         
-         
+                  
         string = body.decode() #encode()
-        print(string)
-        Name_startnum = string.find('"')##从下标0开始，查找在字符串里第一个出现的子串
-        Name_stopnum = string.find('"',Name_startnum+1)##从下标0开始，查找在字符串里第一个出现的子串
-        Value_startnum = string.find(':',Name_stopnum)
-        Value_stopnum = string.find('\n',Value_startnum)
-        Name = body[Name_startnum+1:Name_stopnum];
-        Value = body[Value_startnum+1:Value_stopnum];
-        print(string)
+        string = json.loads(string)
         
-        if (Name==b'Shutdown'):
+        control_ID=string['control_ID']
+        control_val=string['value']
+        
+        if (control_ID=='shutdown' and control_val=='1'):
             response = BytesIO()
             response.write(b'HTTP/1.1 200 OK/Server is shutdown successfully')
             self.wfile.write(response.getvalue())   
@@ -182,7 +177,6 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
             response = BytesIO()
             response.write(b'HTTP/1.1 406 Failed')
             self.wfile.write(response.getvalue())   
- 
  
     def do_PUT(self):
         
@@ -226,48 +220,43 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
             self.wfile.write(response.getvalue())           
 
         Finalsenddata = b'USet_ParkSen_' + senddata1 + senddata2 + senddata3     
-        print(Finalsenddata)
         DataToSimulink.send(Finalsenddata)  
 
-#Create TCP client to connect to Labview            
-address = ('17486633in.iask.in', 36539)   
-DataToSimulink = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
-DataToSimulink.connect(address) 
+if __name__ == '__main__':
+    #Create TCP client to connect to Labview            
+    address = ('localhost',10005)   
+    DataToSimulink = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
+    DataToSimulink.connect(address) 
 
-#Open the channel for publishing the Charing and parking status event of AAU
-hd = {'adapter-id':'AAU_Adapter','infrastructure-id':'RMEMS'}
-url = 'http://localhost:9997/agent/events/ParkingAndChargingStatus'
-r=requests.request('POST',url,headers=hd)
-print(r.text)
+    #Open the channel for publishing the Charing and parking status event of AAU
+    hd = {'adapter-id':'AAU_Adapter','infrastructure-id':'RMEMS'}
+    url = 'http://localhost:9997/agent/events/ParkingAndChargingStatus'
+    r=requests.request('POST',url,headers=hd)
+    print(r.text)
 
+    #subscribe to the event of parking sensor 1(sensor_id:008000000400882f)           
+    hd = {'adapter-id':'AAU_Adapter','infrastructure-id':'RMEMS'}
+    url = 'http://localhost:9997/agent/objects/aa9a9d7a-cf7d-452f-9165-cf038eccb96b/events/sensor-b4be8848-35bd-4720-9158-305d7e5c8c2b'
+    r=requests.request('POST',url,headers=hd)
+    print(r.text)
 
-#subscribe to the event of parking sensor 1(sensor_id:008000000400882f)           
-hd = {'adapter-id':'AAU_Adapter','infrastructure-id':'RMEMS'}
-url = 'http://localhost:9997/agent/objects/aa9a9d7a-cf7d-452f-9165-cf038eccb96b/events/sensor-b4be8848-35bd-4720-9158-305d7e5c8c2b'
-r=requests.request('POST',url,headers=hd)
-print(r.text)
+    #subscribe to the event of parking sensor 2(sensor_id:0080000004008835)           
+    hd = {'adapter-id':'AAU_Adapter','infrastructure-id':'RMEMS'}
+    url = 'http://localhost:9997/agent/objects/22bc0d05-5c35-4042-82bc-9be958929bb4/events/sensor-849da2b0-8ed1-4d3b-bcac-46572b390acf'
+    r=requests.request('POST',url,headers=hd)
+    print(r.text)
 
-#subscribe to the event of parking sensor 2(sensor_id:0080000004008835)           
-hd = {'adapter-id':'AAU_Adapter','infrastructure-id':'RMEMS'}
-url = 'http://localhost:9997/agent/objects/22bc0d05-5c35-4042-82bc-9be958929bb4/events/sensor-849da2b0-8ed1-4d3b-bcac-46572b390acf'
-r=requests.request('POST',url,headers=hd)
-print(r.text)
+    #subscribe to the event of parking sensor 3(sensor_id:008000000400884a)           
+    hd = {'adapter-id':'AAU_Adapter','infrastructure-id':'RMEMS'}
+    url = 'http://localhost:9997/agent/objects/689e8e09-56e2-4a19-8d97-81286f0bedd3/events/sensor-64f41424-93ee-4130-8519-66a250f5bfe3'
+    r=requests.request('POST',url,headers=hd)
+    print(r.text)
 
-#subscribe to the event of parking sensor 3(sensor_id:008000000400884a)           
-hd = {'adapter-id':'AAU_Adapter','infrastructure-id':'RMEMS'}
-url = 'http://localhost:9997/agent/objects/689e8e09-56e2-4a19-8d97-81286f0bedd3/events/sensor-64f41424-93ee-4130-8519-66a250f5bfe3'
-r=requests.request('POST',url,headers=hd)
-print(r.text)
+    #start thread for publish parking event
+    t=threading.Timer(5,Timerfun,()) 
+    t.start()
 
-
-#start thread for publish parking event
-t=threading.Timer(5,Timerfun,()) 
-t.start()
-
-#Start AAU adapter 
-print('AAU adapter start working!')         
-httpd = HTTPServer(('localhost', 9995), SimpleHTTPRequestHandler)
-httpd.serve_forever()
-        
-
-
+    #Start AAU adapter 
+    print('AAU adapter start working!')         
+    httpd = HTTPServer(('localhost', 9995), SimpleHTTPRequestHandler)
+    httpd.serve_forever()
